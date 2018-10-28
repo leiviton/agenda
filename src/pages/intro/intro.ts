dd5beaf2efb4c42dddc92f44f7528b8e0a4fa0d9
João Fagner <fagworio@gmail.com>
2018-10-28 15:12:18 -0300

    Created component IntroSlide

---
+++ src/pages/intro/intro.ts
@@ -0,0 +1,32 @@
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, Slides } from 'ionic-angular';
+import {HomePage} from "../home/home";
+
+/**
+ * Generated class for the IntroPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-intro',
+  templateUrl: 'intro.html',
+})
+export class IntroPage {
+  @ViewChild(Slides) slides: Slides;
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+  goToHome(){
+    this.navCtrl.setRoot(HomePage);
+  }
+  slideToNext() {
+    this.slides.slideNext(); // also not working
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad IntroPage');
+  }
+
+}
